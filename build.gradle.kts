9a2c3beb5e5ad04181536360639bec615e6dc966
Aviral Gupta <aviralgit0384@gmail.com>
2025-09-20 11:22:38 +0530

    seventh commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,24 +20,28 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-   // implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    // implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
-   implementation("org.springframework.security:spring-security-crypto")
+    implementation("org.springframework.security:spring-security-crypto")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("com.cloudinary:cloudinary-http44:1.33.0")
+
+    // Cloudinary (exclude commons-logging to avoid conflict with spring-jcl)
+    implementation("com.cloudinary:cloudinary-http44:1.33.0") {
+        exclude(group = "commons-logging", module = "commons-logging")
+    }
+
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-   // implementation("org.apache.httpcomponents.client5:httpclient5:5.3")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
 
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation( "org.jetbrains.kotlin:kotlin-stdlib")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
+
 
 kotlin {
 	compilerOptions {
